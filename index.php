cd8a836b9fc9ee8e52bf69a331e54626aaa62b88
William Marrujo <willmarrujo@gmail.com>
2014-08-03 11:31:00 -0500

    Initial shell
    very basic, nothing on it yet. seriously, just the first page.

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+	<?php include($_SERVER["DOCUMENT_ROOT"] . "/dependencies/includes/head.php"); ?>
+	<title>Coder Dojo Minnetonka</title>
+    </head>
+    <body>
+	<header>
+	    <h1>Coder Dojo</h1>
+	    <h2>Minnetonka, Minnesota, United States of America</h2>
+	    <?php include($_SERVER["DOCUMENT_ROOT"] . "/dependencies/includes/menu.php"); ?>
+	</header>
+	<article>
+	    
+	</article>
+	<footer>
+	    <?php include($_SERVER["DOCUMENT_ROOT"] . "/dependencies/includes/footer.php"); ?>
+	</footer>
+    </body>
+</html>
